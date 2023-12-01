4045b643fc8a6a9cb115c89b3b9b6f2b79576f3e
Kimberly Hu <kimberlyhu@utexas.edu>
2023-11-30 19:29:47 -0600

    create california dataset

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,24 +1,26 @@
-
-SELECT b.*, r.business_id, r.review_text
+CREATE TABLE california AS
+SELECT b.*, r.review_id, r.review_text
 FROM business b
 LEFT JOIN (
     SELECT *
     FROM (
-        SELECT business_id, review_text FROM review_1
+        SELECT business_id, review_id, review_text FROM review_1
         UNION ALL
-        SELECT business_id, review_text FROM review_2
+        SELECT business_id, review_id, review_text FROM review_2
         UNION ALL
-        SELECT business_id, review_text FROM review_3
+        SELECT business_id, review_id, review_text FROM review_3
         UNION ALL
-        SELECT business_id, review_text FROM review_4
+        SELECT business_id, review_id, review_text FROM review_4
         UNION ALL
-        SELECT business_id, review_text FROM review_5
+        SELECT business_id, review_id, review_text FROM review_5
         UNION ALL
-        SELECT business_id, review_text FROM review_6
+        SELECT business_id, review_id, review_text FROM review_6
         UNION ALL
-        SELECT business_id, review_text FROM review_7
+        SELECT business_id, review_id, review_text FROM review_7
     ) AS combined_reviews
 ) r ON b.business_id = r.business_id
 WHERE b.state = 'CA' AND b.categories LIKE '%Restaurants%'
 ORDER BY b.business_id;
+
+ALTER TABLE california ADD embedding real[];
 
