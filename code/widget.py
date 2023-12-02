dc173d4134d724a2aa29c5725791cfca524cf375
root <root@LAPTOP-PG22RMBN>
2023-12-02 16:06:25 -0600

    minor changes on widgets

--- code/widget.py
+++ code/widget.py
@@ -1,18 +1,19 @@
 import streamlit as st
+from match import match
 
-if "attendance" not in st.session_state:
-    st.session_state.attendance = set()
-
-number = st.number_input("Insert your zipcode", value=None, placeholder="Type a zipcode...")
-st.write('The current zipcode is ', number)
+zipcode = st.number_input("Insert your zipcode", min_value=0, step=1, format="%i")
+st.write('The current zipcode is ', zipcode)
 
 def recommendation():
-    if st.session_state.name in st.session_state.attendance:
-        st.info(f"{st.session_state.name} has already been counted.")
+    user_query = st.session_state.name
+    matching_results = match(user_query, zipcode)
+    if matching_results:
+        for result in matching_results:
+            st.write(result)  
     else:
-        st.session_state.attendance.add(st.session_state.name)
+        st.write("No matching results found.")
 
 with st.form(key="my_form"):
-    st.text_input("Any thoughts on where to eat? Enter your thoughts here and we will find you a restaurant! ", key="name")
-    st.form_submit_button("enter", on_click=recommendation)
+    st.text_input("Any thoughts on where to eat? Enter your thoughts here and we will find you a restaurant!", key="name")
+    st.form_submit_button("Enter", on_click=recommendation)
 
