e90da13541b81d285c7c16efa17af99d56ebb79b
Yalunwang <143123747+YalunWangyw26347@users.noreply.github.com>
2023-12-02 00:24:50 -0600

    Update match.py

--- code/match.py
+++ code/match.py
@@ -1,52 +1,67 @@
 from database import engine
 from sentence_transformers import SentenceTransformer
 from sqlalchemy import text, bindparam
-
 model = SentenceTransformer('all-MiniLM-L6-v2')
-
+conn=engine.connect()
+# conn.execute("CREATE EXTENSION IF NOT EXISTS vector")
+# register_vector(conn)
 
 def match():
-
     user_query = input("Enter your query: ")
     user_embedding = model.encode([user_query])[0]
-    user_embedding_list = user_embedding.tolist()
-    
+    user_embedding_list=user_embedding.tolist()
+    user_embedding_string= str(user_embedding_list)
+    print(user_embedding_string)
     q = """
-        WITH cosine_table AS (
-            SELECT *,
-            (SELECT SUM(a * b) FROM unnest(:user_embedding, embedding::numeric[]) AS s(a,b)) /
-            (SELECT SQRT(SUM(a * a) * SUM(b * b)) FROM unnest(:user_embedding, embedding::numeric[]) AS s(a,b)) AS cos_similarity
-            FROM california
-        )
-        SELECT * FROM cosine_table
-        WHERE cos_similarity > 0.7
-        ORDER BY cos_similarity DESC
-        LIMIT 20;
+    WITH vector_matches AS (
+    SELECT
+        c.review_id,
+        1 - (c.embedding_sample <=> :user_embedding) AS similarity
+    FROM
+        california c
+        --where 1 - (c.embedding_sample <=> :user_embedding)>0.9
+    	ORDER by similarity DESC
+    	LIMIT 1
+)
+
+SELECT
+    c.review_id,
+    c.embedding,
+    c."review_text" 
+FROM
+    california c
+WHERE
+    c.review_id IN (SELECT review_id FROM vector_matches)
         """
-
+   
     with engine.connect() as conn:
-        stmt = text(q).bindparams(bindparam('user_embedding', value=user_embedding_list))
-        result = conn.execute(stmt)
-
+        stmt = text(q) 
+        result = conn.execute(
+                stmt,
+                user_embedding=user_embedding_string)
     matches = []
+
 
     if result.rowcount == 0:
         raise Exception("Did not find any results.")
     else:
-        for r in result:
-            matches.append(r['business_id'])
-
-    return matches
-
+        print("done")
+        # for r in result:
+        # # Convert the tuple to a dictionary
+        #     row_as_dict = {column: value for column, value in zip(result.keys(), r)}
+        # # Now you can access values using column names
+        #     matches.append(f"The name of the restaurant is {row_as_dict['name']}.")
+    return
 
 if __name__ == "__main__":
 
+
     try:
         matches = match()
-        print(matches)
+        for match in matches:
+            print(match)
     except Exception as e:
         print(e)
-
 
 
           
